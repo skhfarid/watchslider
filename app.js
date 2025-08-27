e09c575db6201a5edfeee725de56bf2fc9b7e09e
Md Sheikh Farid <mdsheikhfaridh@gmail.com>
2025-08-27 18:50:40 +0600

    Update app.js

--- app.js
+++ app.js
@@ -1,47 +1,3 @@
-// let nextButton = document.getElementById('next');
-// let prevButton = document.getElementById('prev');
-// let carousel = document.querySelector('.carousel');
-// let listHTML = document.querySelector('.carousel .list');
-// let seeMoreButtons = document.querySelectorAll('.seeMore');
-// let backButton = document.getElementById('back');
-
-// nextButton.onclick = function(){
-//     showSlider('next');
-// }
-// prevButton.onclick = function(){
-//     showSlider('prev');
-// }
-// let unAcceppClick;
-// const showSlider = (type) => {
-//     nextButton.style.pointerEvents = 'none';
-//     prevButton.style.pointerEvents = 'none';
-
-//     carousel.classList.remove('next', 'prev');
-//     let items = document.querySelectorAll('.carousel .list .item');
-//     if(type === 'next'){
-//         listHTML.appendChild(items[0]);
-//         carousel.classList.add('next');
-//     }else{
-//         listHTML.prepend(items[items.length - 1]);
-//         carousel.classList.add('prev');
-//     }
-//     clearTimeout(unAcceppClick);
-//     unAcceppClick = setTimeout(()=>{
-//         nextButton.style.pointerEvents = 'auto';
-//         prevButton.style.pointerEvents = 'auto';
-//     }, 2000)
-// }
-// seeMoreButtons.forEach((button) => {
-//     button.onclick = function(){
-//         carousel.classList.remove('next', 'prev');
-//         carousel.classList.add('showDetail');
-//     }
-// });
-// backButton.onclick = function(){
-//     carousel.classList.remove('showDetail');
-// }
-
-
 let nextButton = document.getElementById('next');
 let prevButton = document.getElementById('prev');
 let carousel = document.querySelector('.carousel');
@@ -94,9 +50,15 @@ let autoSlide = setInterval(() => {
 }, 5000);
 
 // (Optional) Pause on hover
-carousel.addEventListener('mouseenter', () => clearInterval(autoSlide));
-carousel.addEventListener('mouseleave', () => {
-    autoSlide = setInterval(() => {
-        showSlider('next');
-    }, 3000);
+let images = document.querySelectorAll('.carousel .list .item img');
+
+// Pause when mouse enters an image
+images.forEach(img => {
+    img.addEventListener('mouseenter', () => clearInterval(autoSlide));
+    img.addEventListener('mouseleave', () => {
+        autoSlide = setInterval(() => {
+            showSlider('next');
+        }, 3000);
+    });
 });
+
